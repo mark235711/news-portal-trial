0e96ead77ad27ee0cfcc76a7dcbe344226fb5a03
Work <work@Marks-MacBook-Pro.local>
2017-12-01 09:59:14 +1100

    added autosave to editor

---
+++ src/GeneralParameters.js
@@ -0,0 +1,34 @@
+//this file contains variables that can easly be changed all in one place
+
+//used in EditArticle, the number of redux state chages it gets before it updates
+export const AUTOSAVE_CHANGE_LIMIT = 10;
+
+//used in EditArticle, the maximum number of lines the teaser text can have
+export const TEASER_TEXT_MAX_LINES = 4;
+
+//used in EditArticle, the maximum number of characters on one line for the teaser text
+export const TEASER_TEXT_CHAR_LINE_LIMIT = 40;
+
+
+//used in MyEditor, the max number of chars before an update is pushed to the redux state, (also waits for end of word)
+export const MAX_CHAR_NOT_UPDATED = 15;
+
+//used in MyEditor and EDitArticle, the maximum amount of time before an autosave can occur
+export const MAX_WAIT_AUTOSAVE = 10000; //in miliseconds, 1000 = 1 sec
+
+//urls used for different functionality
+
+//urls used in the EditArticle
+export const LOAD_ARTICLE_URL = 'http://homestead.app/loadarticle';
+export const CREATE_ARTICLE_URL = 'http://homestead.app/createarticle';
+export const EDIT_ARTICLE_URL = 'http://homestead.app/editarticle';
+export const DELETE_ARTICLE_URL = 'http://homestead.app/deletearticle';
+export const SUBMIT_ARTICLE_FOR_REVIEW_URL = 'http://homestead.app/submitarticleforreview';
+export const PUBLISH_ARTICLE_URL = 'http://homestead.app/publisharticle';
+export const PUSH_BACK_ARTICLE_URL = 'http://homestead.app/pushbackarticle';
+
+
+//urls used in ViewArticles
+export const VIEW_ALL_ARTICLES_URL = 'http://homestead.app/viewallarticles';
+export const VIEW_ALL_PENDING_ARTICLES_URL = 'http://homestead.app/viewallpendingarticles';
+export const VIEW_ALL_PUBLISHED_ARTICLES_URL = 'http://homestead.app/viewallpublishedarticles';
